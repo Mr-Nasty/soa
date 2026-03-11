7a7d0e32556d3c0e0bee7b4b36484f98ca739235
Mr-Nasty <97503871+Mr-Nasty@users.noreply.github.com>
2026-03-11 08:26:42 +0530

    Create AppConfig class for JAX-RS application

---
+++ prac7(withoutdb)/AppConfig.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.library;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+
+@ApplicationPath("api")
+/**
+ *
+ * @author devan
+ */
+public class AppCongfig extends Application {
+    
+}
